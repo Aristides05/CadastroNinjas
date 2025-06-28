9ba27914197600d77b7e0abe5f1a6e0aff2a81c9
Aristides05 <allancosta1205@gmail.com>
2025-06-27 21:03:56 -0300

    Criacao da entidade: MissaoModel

---
+++ src/main/java/edu/allan/Cadastro/Missoes/MissaoModel.java
@@ -0,0 +1,28 @@
+package edu.allan.Cadastro.Missoes;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class MissaoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name; 
+    private String description; 
+    private String rank;
+    private String status; // "Pendente", "Em Andamento", "Conclu
+
+}
